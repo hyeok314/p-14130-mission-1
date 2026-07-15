6d791dc11697f24b4a003f85df137af9193624af
hyeok314 <joysys8272@gmail.com>
2026-07-15 14:19:30 +0900

    1단계

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,15 @@
+package com
+
+class App {
+    fun run() {
+        println("== 명언 앱 ==")
+
+        while (true) {
+            print("명령) ")
+            val input = readlnOrNull()!!.trim()
+
+            if (input == "종료") break
+
+        }
+    }
+}
