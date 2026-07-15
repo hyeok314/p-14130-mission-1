497b0ff8f95e7256d370cd5305a000604df7f743
hyeok314 <joysys8272@gmail.com>
2026-07-15 14:39:35 +0900

    5단계

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -4,6 +4,7 @@ class App {
     fun run() {
         println("== 명언 앱 ==")
         var lastId = 0
+        val wiseSayings = mutableListOf<WiseSaying>()
 
         while (true) {
             print("명령) ")
@@ -18,8 +19,16 @@ class App {
                 val author = readlnOrNull()!!.trim()
 
                 val id = ++lastId
+                wiseSayings.add(WiseSaying(id, content, author))
 
                 println("${id}번 명언이 등록되었습니다.")
+            }
+            else if(input == "목록") {
+                println("번호 / 작가 / 명언")
+                println("-------------------------")
+                wiseSayings.reversed().forEach {
+                    println("${it.id} / ${it.content} / ${it.author}")
+                }
             }
         }
     }
